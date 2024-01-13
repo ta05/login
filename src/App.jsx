b1c328803e534e168e3bd73aac15bdcf8a580a91
Tolu Alimi <talimi2000@gmail.com>
2024-01-13 16:40:26 -0600

    Created login page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+    return (
+        <>
+            <LoginForm />
+        </>
+    );
+}
+
+export default App;
