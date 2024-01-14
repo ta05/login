e1c2342e51cfc15de9e49d18f27b155c154baf56
Tolu Alimi <talimi2000@gmail.com>
2024-01-13 20:04:46 -0600

    Added handleInput and handleSubmit event listeners to the login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FaLock, FaUser } from "react-icons/fa";
 
 import "../styles/LoginForm.css";
 
 const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleInputChange = (event) => {
+        if (event.target.name === "username") {
+            setUsername(event.target.value);
+        } else if (event.target.name === "password") {
+            setPassword(event.target.value);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <div className="wrapper">
             <form action="">
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required />
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={handleInputChange}
+                        required
+                    />
                     <FaUser className="icon" />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleInputChange}
+                        required
+                    />
                     <FaLock className="icon" />
                 </div>
 
@@ -25,7 +52,9 @@ const LoginForm = () => {
                     </label>
                     <a href="#">Forgot password?</a>
                 </div>
-                <button type="submit">Login</button>
+                <button type="submit" onSubmit={handleSubmit}>
+                    Login
+                </button>
                 <div className="register-link">
                     <p>
                         Don't have an account? <a href="#">Register</a>
